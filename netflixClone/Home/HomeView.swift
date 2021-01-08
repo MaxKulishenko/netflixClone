e434d72da54b91566f31ac04f573aa3319ce1703
MaxKulisenko <63362149+MaxKulisenko@users.noreply.github.com>
2021-01-08 21:32:11 +0200

    add home view file

---
+++ netflixClone/Home/HomeView.swift
@@ -0,0 +1,20 @@
+//
+//  HomeView.swift
+//  netflixClone
+//
+//  Created by Maksym on 08.01.2021.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
