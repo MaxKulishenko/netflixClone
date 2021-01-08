5fcf6e53cd6030d36e4cef6ac7f4628c159591ae
MaxKulisenko <63362149+MaxKulisenko@users.noreply.github.com>
2021-01-08 21:26:34 +0200

    Initial Commit

---
+++ netflixClone/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  netflixClone
+//
+//  Created by Maksym on 08.01.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ netflixClone/netflixCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  netflixCloneApp.swift
+//  netflixClone
+//
+//  Created by Maksym on 08.01.2021.
+//
+
+import SwiftUI
+
+@main
+struct netflixCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
