40284478ae12ca8b7c504c93158cbbcb2855d8ba
MaxKulisenko <63362149+MaxKulisenko@users.noreply.github.com>
2021-01-08 23:18:31 +0200

    add HomeVM file / add scrolling view for category "add now"

--- HomeView.swift
+++ HomeView.swift
@@ -8,11 +8,33 @@
 import SwiftUI
 
 struct  HomeView: View {
+    var vm = HomeVM()
+    
     var body: some View {
         ZStack {
             Color.black
                 .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+            
+            ForEach(vm.allCategories, id: \.self ) { category in
+                VStack {
+                    HStack {
+                        Text(category)
+                        Spacer()
+                    }
+                    
+                    ScrollView(.horizontal, showsIndicators: false) {
+                        HStack{
+                            ForEach(vm.getMovie(forCat: category)) { movie in
+                                StandardHomeMovie(movie: movie)
+                                    .frame(width: 100 , height: 200)
+                                    .padding(.horizontal, 20)
+                            }
+                        }
+                    }
+                }
+            }
         }
+        .foregroundColor(.white)
     }
 }
 
---
+++ netflixClone/HomeVM.swift
@@ -0,0 +1,34 @@
+//
+//  HomeVM.swift
+//  netflixClone
+//
+//  Created by Maksym on 08.01.2021.
+//
+
+import Foundation
+
+class HomeVM: ObservableObject {
+    
+    // String == Category
+    @Published var movies: [String: [Movie]] = [:]
+    
+    public var allCategories: [String] {
+         movies.keys.map({ String($0) })
+    }
+    
+    public func getMovie(forCat cat: String) -> [Movie] {
+        return movies[cat] ?? []
+    
+    }
+    
+    init() {
+        setupMovies()
+    }
+    
+    func setupMovies() {
+        movies["Trending Now"] = [exampleMovie1]
+    }
+    
+}
+
+
