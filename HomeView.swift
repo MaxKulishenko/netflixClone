7585e35e61f5a464b7ed664fdbc80907a7249fba
MaxKulisenko <63362149+MaxKulisenko@users.noreply.github.com>
2021-01-12 23:32:19 +0200

    add vertical scrollView for categories

--- HomeView.swift
+++ HomeView.swift
@@ -15,20 +15,22 @@ struct  HomeView: View {
             Color.black
                 .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
             //main v-stack
-            LazyVStack {
-                ForEach(vm.allCategories, id: \.self ) { category in
-                    VStack {
-                        HStack {
-                            Text(category)
-                            Spacer()
-                        }
-                        
-                        ScrollView(.horizontal, showsIndicators: false) {
-                            HStack{
-                                ForEach(vm.getMovie(forCat: category)) { movie in
-                                    StandardHomeMovie(movie: movie)
-                                        .frame(width: 100 , height: 200)
-                                        .padding(.horizontal, 20)
+            ScrollView(/*@START_MENU_TOKEN@*/.vertical/*@END_MENU_TOKEN@*/, showsIndicators: false)             {
+                LazyVStack {
+                    ForEach(vm.allCategories, id: \.self ) { category in
+                        VStack {
+                            HStack {
+                                Text(category)
+                                Spacer()
+                            }
+                            
+                            ScrollView(.horizontal, showsIndicators: false) {
+                                LazyHStack{
+                                    ForEach(vm.getMovie(forCat: category)) { movie in
+                                        StandardHomeMovie(movie: movie)
+                                            .frame(width: 100 , height: 200)
+                                            .padding(.horizontal, 20)
+                                    }
                                 }
                             }
                         }
