b597bc295f11c8d1172a5a40c9ce270088bd2dbf
MaxKulisenko <63362149+MaxKulisenko@users.noreply.github.com>
2021-02-01 19:06:14 +0200

    make homeView image more lighter

--- netflixClone/TopMoviePreview.swift
+++ netflixClone/TopMoviePreview.swift
@@ -5,41 +5,41 @@
 //  Created by Maksym on 13.01.2021.
 //
 
-import SwiftUI
 import KingfisherSwiftUI
+import SwiftUI
 
 struct TopMoviePreview: View {
     var movie: Movie
-    
+
     func isCategoryLast(_ cat: String) -> Bool {
         let catCount = movie.categories.count
-        
+
         if let index = movie.categories.firstIndex(of: cat) {
             if index + 1 != catCount {
                 return false
             }
         }
-        
+
         return true
     }
-    
+
     var body: some View {
         ZStack {
             KFImage(movie.thumbnailURL)
                 .resizable()
                 .scaledToFill()
                 .clipped()
-            
+
             VStack {
                 Spacer()
-                
+
                 HStack {
                     ForEach(movie.categories, id: \.self) { category in
-                        
+
                         HStack {
                             Text(category)
                                 .font(.footnote)
-                            
+
                             if !isCategoryLast(category) {
                                 Image(systemName: "circle.fill")
                                     .foregroundColor(.blue)
@@ -48,32 +48,37 @@ struct TopMoviePreview: View {
                         }
                     }
                 }
-                
+
                 HStack {
                     Spacer()
-                    
+
                     SmallVerticalButton(text: "My List", isOnImage: "checkmark", isOffImage: "plus", isOn: true) {
                         //
                     }
-                    
+
                     Spacer()
-                    
+
                     WhitePauseButton(text: "Play", imageName: "play.fill") {
                         //
                     }
                     .frame(width: 120)
-                    
+
                     Spacer()
-                    
+
                     SmallVerticalButton(text: "Info", isOnImage: "info.circle", isOffImage: "info.circle", isOn: true) {
                         //
                     }
-                    
+
                     Spacer()
                 }
             }
-            .background(LinearGradient(gradient: Gradient(colors: [Color.black.opacity(0.0), Color.black.opacity(0.95)]), startPoint: .top, endPoint: .bottom))
-            
+            .background(
+                LinearGradient(
+                    gradient: Gradient(colors: [Color.black.opacity(0.0),   Color.black.opacity(0.95)]),
+                    startPoint: .top,
+                    endPoint: .bottom)
+            )
+            .padding(.top, 250)
         }
         .foregroundColor(.white)
     }
