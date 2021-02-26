fd3dffeb7008a6ae46f834f402fef15cd4903404
MaxKulisenko <63362149+MaxKulisenko@users.noreply.github.com>
2021-02-26 21:36:00 +0200

    create view for movie detail / create close button / create picture and add information about movie in view

---
+++ netflixClone/MovieDetail.swift
@@ -0,0 +1,74 @@
+//
+//  MovieDetail.swift
+//  netflixClone
+//
+//  Created by Maksym on 26.02.2021.
+//
+
+import SwiftUI
+
+struct MovieDetail: View {
+    var movie: Movie
+    
+    let screen = UIScreen.main.bounds
+    var body: some View {
+        ZStack {
+            Color.black
+                .edgesIgnoringSafeArea(.all)
+            
+            VStack {
+                HStack {
+                    Spacer()
+                    
+                    Button(action: {
+                        // close view
+                    }, label: {
+                        Image(systemName: "xmark.circle")
+                            .font(.system(size: 28))
+                    })
+                }.padding(.horizontal, 22)
+                
+                ScrollView(.vertical, showsIndicators: false) {
+                    
+                    VStack {
+                        
+                        StandardHomeMovie(movie: movie)
+                            .frame(width: screen.width / 2.5)
+                        
+                        MoviewInfoSubheadline(movie: movie)
+                    }
+                }
+                
+                Spacer()
+            }
+            .foregroundColor(.white)
+            
+        }
+    }
+}
+
+struct MovieDetail_Previews: PreviewProvider {
+    static var previews: some View {
+        MovieDetail(movie: exampleMovie3)
+    }
+}
+
+struct MoviewInfoSubheadline: View {
+    var movie: Movie
+    
+    var body: some View {
+        HStack {
+            Image(systemName: "hand.thumbsup.fill")
+                .foregroundColor(.white)
+            
+            Text("Movie Year")
+            
+            Text("Rating")
+            
+            Text("Seasons")
+            
+        }
+        .foregroundColor(.gray)
+        .padding(.vertical, 6)
+    }
+}
