bf17bf53f222e744b58cc8409e8e8e913f301919
MaxKulisenko <63362149+MaxKulisenko@users.noreply.github.com>
2021-03-04 18:17:41 +0200

    create swift model for episodes

---
+++ netflixClone/Model/ Episode.swift
@@ -0,0 +1,22 @@
+//
+//   Episode.swift
+//  netflixClone
+//
+//  Created by Maksym on 01.03.2021.
+//
+
+import Foundation
+
+struct Episode: Identifiable {
+    var id = UUID().uuidString
+    
+    var name: String
+    var season: Int
+    var thumbnailImageURLString: String
+    var description: String
+    var length: Int
+    
+    var thumbnailURL: URL {
+        return URL(string: thumbnailImageURLString)!
+    }
+}
