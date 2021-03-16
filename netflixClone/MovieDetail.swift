7556acf8f82042560e7dc84759f974fba94f0cdd
MaxKulisenko <63362149+MaxKulisenko@users.noreply.github.com>
2021-03-16 16:38:49 +0200

    add "play" button

--- netflixClone/MovieDetail.swift
+++ netflixClone/MovieDetail.swift
@@ -42,6 +42,10 @@ struct MovieDetail: View {
                                 .bold()
                                 .font(.headline)
                         }
+                        
+                        PlayButton(text: "Play", imageName: "play.fill", backgroundColor: .red) {
+                            //
+                        }
                     }
                 }
                 
